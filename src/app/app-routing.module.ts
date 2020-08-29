a7feb595a8a444e1fd4c51b69dc51f3244f7139d
FalconHeart <62263841+FalconHeart@users.noreply.github.com>
2020-08-29 17:19:58 +0200

    Added UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent }   from './components/landing/landing.component';
+import { MainAppComponent }   from './components/main-app/main-app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent},
+  { path: 'main-app', component: MainAppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LandingComponent, MainAppComponent]
